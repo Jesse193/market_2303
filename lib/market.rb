c314839522e82f8d0857a4f07c247fd3ffa5db6d
Jesse Sorman <jessesorman@gmail.com>
2023-04-17 12:28:43 -0400

    feat market can return vendor names

--- lib/market.rb
+++ lib/market.rb
@@ -7,4 +7,11 @@ class Market
   def add_vendor(vendor)
     @vendors << vendor
   end
+  def vendor_names
+    names = []
+    @vendors.each do |vendor|
+      names << vendor.name
+    end
+    return names
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -23,5 +23,7 @@ RSpec.describe Market do
     market.add_vendor(vendor2)
     market.add_vendor(vendor3)
     expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+    expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+
   end
 end
