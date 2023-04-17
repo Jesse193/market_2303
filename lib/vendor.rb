1fe7162093d3bb29395f9bd3ca1dd2312ad822cc
Jesse Sorman <jessesorman@gmail.com>
2023-04-17 12:09:26 -0400

    vendor can stock items

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,14 +1,17 @@
 class Vendor
-  attr_reader :name, :inventory
+  attr_reader :name, :inventory, :stock
   def initialize(name)
     @name = name
     @inventory = {}
   end
   def check_stock(item)
-    if @inventory.include?(item)
-      item.count
+    if @inventory.keys.include?(item)
+      @inventory[item]
     else
       0
     end
+  end
+  def stock(item, qnty)
+    @inventory.merge!(item => qnty)
   end
 end
